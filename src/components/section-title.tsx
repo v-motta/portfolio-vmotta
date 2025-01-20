6f490231938790fa6b48550ca66cb9c51ba5644f
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-20 21:05:32 +0100

    feat: removing section title

--- src/components/section-title.tsx
+++
@@ -1,7 +0,0 @@
-export function SectionTitle({ children }: { children: React.ReactNode }) {
-  return (
-    <h1 className="concat-variable text-center font-bold text-4xl">
-      {children}
-    </h1>
-  )
-}
