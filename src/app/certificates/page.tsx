bb467c2be7b0c6fa6e198953b465826ef62b8076
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-21 15:25:20 +0100

    fix: responsive issues

--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -3,7 +3,7 @@ import { CertificateCard } from './card'
 
 export default function ProjectsPage() {
   return (
-    <main className="px-6 pt-10 lg:min-h-[calc(100vh-6rem-4rem)] xl:px-16 2xl:px-36">
+    <main className="px-6 pt-10 lg:min-h-[calc(100vh-6rem-6rem)] xl:px-16 2xl:px-36">
       <h1 className="section-title">Certificates</h1>
       <div className="grid gap-5 py-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {certificates.map(certificate => (
