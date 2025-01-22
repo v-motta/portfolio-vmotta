5b36c8aa5779830a4814b790a324a314e5dbcdfe
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-22 20:55:21 +0100

    feat: certificates from database

--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -1,12 +1,16 @@
-import { certificates } from '@/app/data.json'
+import { prisma } from '@/lib/db-client'
 import { CertificateCard } from './card'
 
-export default function ProjectsPage() {
+export default async function CertificatesPage() {
+  const allCertificates = await prisma.certificate.findMany({
+    orderBy: { issueDate: 'desc' },
+  })
+
   return (
     <main className="px-6 pt-10 lg:min-h-[calc(100vh-6rem-6rem)] xl:px-16 2xl:px-36">
       <h1 className="section-title">Certificates</h1>
       <div className="grid gap-5 py-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-        {certificates.map(certificate => (
+        {allCertificates.map(certificate => (
           <CertificateCard key={certificate.id} certificate={certificate} />
         ))}
       </div>
