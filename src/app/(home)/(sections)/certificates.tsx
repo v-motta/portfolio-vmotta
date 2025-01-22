d8520248d64504c214619dd1903ca6bb7973a367
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-22 11:48:48 +0100

    feat: latest three certificates from database

--- src/app/(home)/(sections)/certificates.tsx
+++ src/app/(home)/(sections)/certificates.tsx
@@ -1,16 +1,21 @@
 import { CertificateCard } from '@/app/certificates/card'
-import { certificates } from '@/app/data.json'
 import { Button } from '@/components/ui/button'
+import { prisma } from '@/lib/db-client'
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 
-export function Certificates() {
+export async function Certificates() {
+  const latestThreeCertificates = await prisma.certificate.findMany({
+    orderBy: { issueDate: 'desc' },
+    take: 3,
+  })
+
   return (
     <section id="certificates" className="flex flex-col gap-8 py-8 md:py-16">
       <h1 className="section-title">Latest Certificates</h1>
 
       <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
-        {certificates.slice(0, 3).map(certificate => (
+        {latestThreeCertificates.map(certificate => (
           <CertificateCard key={certificate.id} certificate={certificate} />
         ))}
       </div>
