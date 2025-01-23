01983310c2006921333d64ccec1bfd46978bfe4b
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-23 23:37:23 +0100

    fix: responsivity of device

--- src/app/(home)/(sections)/main.tsx
+++ src/app/(home)/(sections)/main.tsx
@@ -7,9 +7,9 @@ export function Main() {
       id="main"
       className="relative flex w-full flex-col-reverse items-center justify-center gap-16 py-10 lg:min-h-[calc(100vh-6rem)] lg:flex-row lg:justify-between"
     >
-      <div className="flex flex-col justify-center gap-8 text-center md:text-start">
+      <div className="flex flex-col justify-center gap-8 text-center lg:text-start">
         <div>
-          <h1 className="font-bold font-jet-brains-mono text-3xl xl:text-4xl 2xl:text-5xl">
+          <h1 className="mb-4 font-bold font-jet-brains-mono text-3xl sm:mb-0 xl:text-4xl 2xl:text-5xl">
             Hi, I am <span className="concat-variable">Vinicius Motta</span>
           </h1>
           <h2 className="font-jet-brains-mono font-semibold text-xl 2xl:text-3xl">
