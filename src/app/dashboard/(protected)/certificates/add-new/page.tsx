b5dbd152fdd3b15904ecc6cde1b768add9df86a1
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-24 01:33:44 +0100

    revert

--- src/app/dashboard/(protected)/certificates/add-new/page.tsx
+++ src/app/dashboard/(protected)/certificates/add-new/page.tsx
@@ -1,16 +1,6 @@
 import { AddNewCertificateForm } from './add-new-certificate-form'
 
 export default async function AddNewCertificatePage() {
-  const response = await fetch('http://localhost:3000/api/s3/presigned', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-  }).then(res => res.json())
-
-  console.log(response)
-
   return (
     <div className="mx-auto flex w-1/3 flex-col gap-10 py-10">
       <h1 className="text-center font-jet-brains-mono text-2xl">
