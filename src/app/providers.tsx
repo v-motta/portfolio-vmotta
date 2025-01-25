46ead68c6f2a5a53d625b30f8e1b55994b2c9169
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-25 02:15:13 +0100

    feat: react query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,16 +1,20 @@
 'use client'
 
+import { queryClient } from '@/lib/query-client'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
 import type { ReactNode } from 'react'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
