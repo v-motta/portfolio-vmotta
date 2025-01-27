3a427c85890fddbe147ec69153d428fa5c81b6f7
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-28 00:02:54 +0100

    feat: open dialog to preview certificate

--- src/app/certificates/card.tsx
+++ src/app/certificates/card.tsx
@@ -1,8 +1,17 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 import { Trash2 } from 'lucide-react'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export interface CardCertificateProps {
   certificate: {
@@ -15,51 +24,76 @@ export interface CardCertificateProps {
     createdAt: Date
     technologyId: string
   }
-  onDelete?: (id: string) => void
+  handleOnDelete?: (id: string) => void
 }
 
 export function CertificateCard({
   certificate,
-  onDelete,
+  handleOnDelete,
 }: CardCertificateProps) {
-  return (
-    <div className="relative flex flex-col gap-4 rounded-2xl border border-zinc-400 p-4 shadow dark:border-zinc-700">
-      {onDelete && (
-        <Button
-          size="icon"
-          variant="destructive"
-          className="absolute right-5 bottom-5"
-          onClick={() => onDelete(certificate.id)}
-        >
-          <Trash2 />
-        </Button>
-      )}
+  const [openDialog, setOpenDialog] = useState(false)
 
-      <Image
-        src={certificate.imageUrl}
-        alt=""
-        width={1000}
-        height={1000}
-        quality={100}
-        priority
-        className="aspect-video rounded-lg border border-zinc-400 object-cover object-center dark:border-zinc-700"
-      />
-      <div className="text-center lg:text-start">
-        <h1 className="mb-2 line-clamp-2 h-14 font-bold font-mono text-xl">
-          {certificate.title}
-        </h1>
-        <h2 className="font-semibold text-zinc-600 dark:text-zinc-400">
-          {certificate.company}
-        </h2>
-        <p className="text-zinc-600 dark:text-zinc-400">
-          Issued:{' '}
-          {new Intl.DateTimeFormat('pt-BR', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-          }).format(new Date(certificate.issueDate))}
-        </p>
-      </div>
-    </div>
+  return (
+    <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+      <DialogTrigger asChild>
+        <div className="relative flex cursor-pointer flex-col gap-4 rounded-2xl border border-zinc-400 p-4 shadow dark:border-zinc-700">
+          {handleOnDelete && (
+            <Button
+              size="icon"
+              variant="destructive"
+              className="absolute right-5 bottom-5"
+              onClick={() => handleOnDelete(certificate.id)}
+            >
+              <Trash2 />
+            </Button>
+          )}
+
+          <Image
+            src={certificate.imageUrl}
+            alt=""
+            width={1000}
+            height={1000}
+            quality={100}
+            priority
+            className="aspect-video rounded-lg border border-zinc-400 object-cover object-center dark:border-zinc-700"
+          />
+          <div className="text-center lg:text-start">
+            <h1 className="mb-2 line-clamp-2 h-14 font-bold font-mono text-xl">
+              {certificate.title}
+            </h1>
+            <h2 className="font-semibold text-zinc-600 dark:text-zinc-400">
+              {certificate.company}
+            </h2>
+            <p className="text-zinc-600 dark:text-zinc-400">
+              Issued:{' '}
+              {new Intl.DateTimeFormat('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              }).format(new Date(certificate.issueDate))}
+            </p>
+          </div>
+        </div>
+      </DialogTrigger>
+      <DialogOverlay />
+      <DialogContent className="max-w-[60vw]">
+        {certificate && (
+          <>
+            <DialogTitle>{certificate.title}</DialogTitle>
+            <DialogDescription>
+              Issued by {certificate.company} on{' '}
+              {new Date(certificate.issueDate).toLocaleDateString()}
+            </DialogDescription>
+            <Image
+              src={certificate.imageUrl}
+              alt={certificate.title}
+              width={1920}
+              height={1080}
+              className="h-full rounded-lg shadow-xl"
+            />
+          </>
+        )}
+      </DialogContent>
+    </Dialog>
   )
 }
--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -9,6 +9,7 @@ export default async function CertificatesPage() {
   return (
     <main className="px-6 pt-10 lg:min-h-[calc(100vh-6rem-4rem)] xl:px-16 2xl:px-36">
       <h1 className="section-title">Certificates</h1>
+
       <div className="grid gap-5 py-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {allCertificates.map(certificate => (
           <CertificateCard key={certificate.id} certificate={certificate} />
