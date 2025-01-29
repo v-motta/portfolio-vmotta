31c3f21c22621e0e9602e9795797019adb845f4f
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-01-29 20:03:56 +0100

    fix: layout main section

--- src/app/(home)/(sections)/main.tsx
+++ src/app/(home)/(sections)/main.tsx
@@ -7,15 +7,17 @@ export function Main() {
       id="main"
       className="relative flex w-full flex-col-reverse items-center justify-center gap-16 py-10 lg:min-h-[calc(100vh-6rem)] lg:flex-row lg:justify-between"
     >
-      <div className="flex flex-col justify-center gap-8 text-center lg:text-start">
-        <div>
-          <h1 className="mb-4 font-bold font-jet-brains-mono text-3xl sm:mb-0 xl:text-4xl 2xl:text-5xl">
-            Hi, I am <span className="concat-variable">Vinicius Motta</span>
-          </h1>
-          <h2 className="font-jet-brains-mono font-semibold text-xl 2xl:text-3xl">
-            Full-Stack Developer
-          </h2>
-        </div>
+      <div className="flex flex-col justify-center gap-6 text-center md:gap-8 lg:text-start">
+        <h1 className="flex flex-col gap-1 font-bold font-jet-brains-mono text-2xl md:gap-2 md:text-3xl">
+          Hi, I am
+          <span className="text-3xl xl:text-4xl 2xl:text-5xl">
+            Vinicius Motta
+          </span>
+        </h1>
+
+        <h2 className="concat-variable font-bold text-2xl 2xl:text-3xl">
+          Full-Stack Developer
+        </h2>
 
         <p className="text-balance text-zinc-700 dark:text-zinc-400">
           Currently graduating a degree in Computer Science, with professional
@@ -24,15 +26,15 @@ export function Main() {
         </p>
       </div>
 
-      <div className="content-center items-center">
+      <div className="content-center items-center rounded-full bg-[#275AAE]">
         <Image
-          src="/myself.webp"
+          src="/image.png"
           priority
           width={384}
           height={384}
           quality={100}
           alt="A picture of myself"
-          className="aspect-square w-56 rounded-xl object-cover shadow-[0px_0px_20px] shadow-zinc-400 lg:w-auto dark:shadow-zinc-600"
+          className="aspect-square w-56 rounded-full object-cover shadow-[0px_0px_20px] shadow-zinc-400 lg:w-auto dark:shadow-zinc-600"
         />
       </div>
 
