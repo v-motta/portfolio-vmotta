2661458ac3e789d227e44d7c0642bcf009fcc61f
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-02-13 17:44:49 +0100

    fix: global connection prisma

--- src/lib/db-client.ts
+++ src/lib/db-client.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: ['warn'],
 })
+
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
